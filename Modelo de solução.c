e9e95c5e263bfae49e87f7071b8709a0a05254c0
Luiz-codes <lhm.cerqueira1@gmail.com>
2025-03-25 19:39:51 -0300

    Add files via upload

---
+++ Modelo de solução.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<locale.h>
+#include<stdlib.h>
+
+#define MAX_USUARIOS 100 
+
+struct DadosUsuarios
+{
+	char nome[MAX_USUARIO][999];
+	int  idade[MAX_USUARIOS];
+	float altura[MAX_USUARIOS];
+	int diaNascimento[MAX_USUARIOS];
+	int mesNascimento[MAX_USUARIOS];
+	int anoNascimento[MAX_USUARIOS];
+	char sexo[MAX_USUARIOS][999];
+	char estadoCivil[MAX_USUARIOS][999];
+	char curso[MAX_USUARIOS][999];
+	char matricula[MAX_USUARIOS][999];
+	char endereco[MAX_USUARIOS][999];
+	char materias[MAX_USUARIOS][999];	
+};
+
+int main()
+{
+	
+}
